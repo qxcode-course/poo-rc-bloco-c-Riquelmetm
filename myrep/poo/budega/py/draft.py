85470a4fa0f7095fd3bdab1781c039aaa6bdde06
Riquelmetm <140022375+Riquelmetm@users.noreply.github.com>
2025-11-14 01:42:31 +0000

    budega

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -9,9 +9,26 @@ class Pessoa:
         return f"{self.__nome}"
     
 
+
 class Mercantil:
     def __init__(self, qtdcaixa:int):
-        self.caixa = qtdcaixa
-        self.qtdcaixa = [None] * qtdcaixa
-        self.fila = [None]
+        self.caixa: list[Pessoa | None] = [None] *  qtdcaixa
+        self.qtdcaixa = qtdcaixa
+        self.fila: list[Pessoa | None] = [None]
+        
+    def __str__(self) -> str:
+        caixas: list[str] = list(map(lambda pessoa: '----' if pessoa is None else pessoa.getnome(), self.caixa))
+        strCaixasFinal: str = "[" + ", ".join(caixas) + "]"
+        fila: list[str] = list(map(lambda fila: '----' if fila is None else fila.getnome(), self.fila))
+        strFilaFinal: str = "[" +", ".join(fila) + "]"
+        return f"Caixa: {strCaixasFinal}\nEspera: {strFilaFinal}"
+    
+    def 
+    
+    def adicionar (self, nome: Pessoa | None, posicao: int ):
+        if self.caixa[posicao] is None:
+            self.caixa[posicao] = nome
+        else:
+           print("ja tem gente")
+        return 
         
