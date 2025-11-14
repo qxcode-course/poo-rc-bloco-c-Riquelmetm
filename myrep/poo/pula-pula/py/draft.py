f293e635be06c3f13421c939c0d4840e57afe174
Riquelmetm <140022375+Riquelmetm@users.noreply.github.com>
2025-11-14 21:24:06 +0000

    pulapula

--- myrep/poo/pula-pula/py/draft.py
+++ myrep/poo/pula-pula/py/draft.py
@@ -2,8 +2,11 @@ class Pessoa:
     def __init__(self, nome:str ,idade:int):
         self.nome = nome
         self.idade = idade
+    def getnome (self):
+        return self.nome
     def __str__(self) -> str:
-        return f"{self.nome}:{self.idade}"
+
+        return f"[{getnome}:{self.idade}]"
 class Pula:
     def __init__(self):
         self.brincando: list[Pessoa] = []
@@ -20,7 +23,29 @@ class Pula:
     def Sair(self):
         sair = self.brincando.pop(0)
         self.fila.append(sair)
+        return
 
-    def Remover (self):
+    def Remover (self, nome: str):
+        if True:
+            for i in range (len(self.brincando)):
+                crianca = self.brincando[i]
+                if crianca.getnome() == nome:
+                    self.brincando.pop(i)
+                    return
+            print(f"fail: {nome} nao esta no pula-pula")
+            return
         
+        else:
+            for i in range (len(self.fila)):
+                crianca = self.fila[i]
+                if crianca.getnome() == nome:
+                    self.fila.pop(i)
+                    return
+            print(f"fail: {nome} nao esta na fila")
+            return
+
+
         
+
+    
+
