be299c0914e99b8c449581be4770a2b134079bcd
Riquelmetm <140022375+Riquelmetm@users.noreply.github.com>
2025-11-14 03:57:50 +0000

    budega

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -46,9 +46,30 @@ class Mercantil:
         else:
             print("fail: caixa inexistente")
 
+def main():
+    mercantil = None
 
+    while True:
+        line: str = input()
+        print("$" + line)
+        args: list[str] = line.split(" ")
 
+        if args[0] == "show":
+            print(mercantil)
 
+        elif args[0] ==  "init":
+            qtdcaixa = int(args[1])
+            mercantil = Mercantil(qtdcaixa)
+        elif args[0] ==  "enter":
+            nome = args[1]
+            mercantil.chegar(nome)
 
+        elif args[0] ==  "call":
+            mercantil.chamar()
+        
+        elif args[0] ==  "finish":
+            mercantil.finalizar()
 
         
+
+
