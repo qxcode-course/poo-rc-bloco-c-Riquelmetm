3e289f6e75faf4d8fdf3de3457e083a1dbe3890e
Riquelmetm <140022375+Riquelmetm@users.noreply.github.com>
2025-11-19 00:24:54 +0000

    array

---
+++ myrep/poo/array/py/draft.py
@@ -0,0 +1,31 @@
+class lista:
+    def __init__(self):
+        self.lista = []
+
+    def remover(self, args[1]):
+        if not self.lista:
+            print("Lista vazia")
+            return
+        else:
+            if args[1]
+                
+    def adicionar (self, valor):
+        
+        a = int(input("Digite 1 para inserir normalmente, Digite 2 para inserir em uma posicao especifica: "))
+        if a == 1:
+            self.lista.append(valor)
+        else:
+            posicao = int(input("Digite a posicao do valor"))
+            self.lista.insert(posicao, valor)
+    
+    def remover (self):
+        
+    
+    
+    
+    
+    
+    
+
+        
+    
